ff98b188d07b615c5660fbfd342e32156dbd0df9
Marco Mettke <mrm@tradebyte.biz>
2018-09-19 09:09:11 +0200

    add files

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Default</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- font-awesome-icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome-icons -->
+<link href="//fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i,800" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/script.js"></script>
+
+</head>
+	
+<body>
+	<h1>Servus, it works !</h1>
+</body>
+</html>
